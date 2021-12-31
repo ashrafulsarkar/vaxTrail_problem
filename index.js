8f271ae85ebb71e24440031ca3239748d9b0121b
ashrafulsarkar <ashrafulsarkar47@gmail.com>
2021-12-31 23:09:07 +0600

    sort add

--- index.js
+++ index.js
@@ -11,16 +11,28 @@ function vaxTrail(data) {
             if (age>=20 && age<=30) {
                 const value = { name: name, age: age, temperature: temperature };
                 dataA.push(value);
+                dataA.sort(function (a, b) {
+                    return a.age % 2 - b.age % 2;
+                });
             }else if(age>=31 && age<=40){
                 const value = { name: name, age: age, temperature: temperature };
                 dataB.push(value);
+                dataB.sort(function (a, b) {
+                    return a.age % 2 - b.age % 2;
+                });
             }else if(age>=41 && age<=50){
                 const value = { name: name, age: age, temperature: temperature };
                 dataC.push(value);
+                dataC.sort(function (a, b) {
+                    return a.age % 2 - b.age % 2;
+                });
             }
         }else{
             const value = { name: name, age: age, temperature: temperature };
             dataD.push(value);
+            dataD.sort(function (a, b) {
+                return a.age % 2 - b.age % 2;
+            });
         }
     }
     finalObject = { A: dataA, B: dataB, C: dataC, D: dataD };
